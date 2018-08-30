0235299a4769620024d24cad5e0a61f6b4ad5e44
vaniot <vaniot@yeah.net>
2018-08-30 14:38:09 +0800

    modify

--- 1.two-sum.cpp
+++ 1.two-sum.cpp
@@ -28,12 +28,9 @@ public:
         unordered_map<int, int> m;
         vector<int> result;
         for(int i=0; i<nums.size(); i++){
-            // not found the second one
             if (m.find(nums[i])==m.end() ) { 
-                // store the first one poisition into the second one's key
                 m[target - nums[i]] = i; 
             }else { 
-                // found the second one
                 result.push_back(m[nums[i]]);
                 result.push_back(i);
                 break;
