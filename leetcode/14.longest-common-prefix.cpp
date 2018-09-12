43f25eda590dea39d6733cf47546f0c232134721
vaniot <vaniot@yeah.net>
2018-09-12 21:35:11 +0800

    leetcode 14

---
+++ leetcode/14.longest-common-prefix.cpp
@@ -0,0 +1,51 @@
+/*
+ * [14] Longest Common Prefix
+ *
+ * https://leetcode-cn.com/problems/longest-common-prefix/description/
+ *
+ * algorithms
+ * Easy (28.93%)
+ * Total Accepted:    19.5K
+ * Total Submissions: 67.5K
+ * Testcase Example:  '["flower","flow","flight"]'
+ *
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+ * 
+ * 如果不存在公共前缀，返回空字符串 ""。
+ * 
+ * 示例 1:
+ * 
+ * 输入: ["flower","flow","flight"]
+ * 输出: "fl"
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: ["dog","racecar","car"]
+ * 输出: ""
+ * 解释: 输入不存在公共前缀。
+ * 
+ * 
+ * 说明:
+ * 
+ * 所有输入只包含小写字母 a-z 。
+ * 
+ */
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string common;
+        if(strs.size()<=0){ //第一个字符串是否为空 注意边界问题！！
+            return common;
+        }
+        for(int i=0;i<strs[0].size();i++){//
+            for(int j=1;j<strs.size();j++){
+                if(strs[j][i]!=strs[0][i]){
+                    return common;
+                }
+            }
+            common+=strs[0][i];
+        }
+        return common;
+    }
+};
