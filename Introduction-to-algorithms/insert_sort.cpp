144325d761afbada33afe27e898af567f72d3322
vaniot <vaniot@yeah.net>
2018-09-13 23:40:30 +0800

    leetcode:20 26

--- Introduction-to-algorithms/insert_sort.cpp
+++ Introduction-to-algorithms/insert_sort.cpp
@@ -30,6 +30,25 @@ int main(){
     end_t=clock();
     std::cout<<(end_t-start_t)/(CLOCKS_PER_SEC*60)<<"minutes"<<std::endl;
 
+    //数据赋值计时
+    start_t=clock();
+    //利用随机数生成0.0到0.1之间的实数
+    std::default_random_engine generator(time(NULL));
+    std::uniform_real_distribution<double> distribution(0.0,1.0);
+    for (size_t i= 0; i<V.size() ; ++i) {
+        V[i]=distribution(generator);
+    }
+    //数据赋值计时结束并输出时间
 
+    end_t=clock();
+    std::cout<<(end_t-start_t)/(CLOCKS_PER_SEC*60)<<"minutes"<<std::endl;
 
+    //排序计时开始
+    start_t=clock();
+    //插入排序
+    insert_sort(V);
+    //排序计时结束并输出时间
+    end_t=clock();
+    std::cout<<(end_t-start_t)/(CLOCKS_PER_SEC*60)<<"minutes"<<std::endl;
+    return 0;
 }
