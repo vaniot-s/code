ce47c40bda84442a96620dc22b96b106dfb78ee3
Vaniot <vaniot@yeah.net>
2019-01-08 19:57:51 +0800

    334

---
+++ leetcode/344.Reverse String/main.h
@@ -0,0 +1,43 @@
+//
+// Created by vaniot on 19-1-8.
+//
+/**
+ * 编写一个函数，其作用是将输入的字符串反转过来。
+
+示例 1:
+
+输入: "hello"
+输出: "olleh"
+
+示例 2:
+
+输入: "A man, a plan, a canal: Panama"
+输出: "amanaP :lanac a ,nalp a ,nam A"
+
+思路:对撞指针
+ */
+
+#ifndef NOTES_MAIN_H
+#define NOTES_MAIN_H
+
+class Solution {
+public:
+    string reverseString(string s) {
+//        if (!s.length()) //容错
+//            return "";
+//        int r=s.length()-1;
+//        for (int i = 0; i <=(s.length()-1)/2; ++i) {
+//            char k=s[i];
+//            s[i]=s[r-i];
+//            s[r-i]=k;
+//        }
+//        return s;
+//        int i = 0,j = s.length() - 1;
+//        while(i <= j) {
+//            swap(s[i++],s[j--]);
+//        }
+//        return s;
+    }
+};
+
+#endif //NOTES_MAIN_H
